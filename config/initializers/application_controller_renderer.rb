33282a9555035b74227a68829569b4acad421f44
nkwada <n.qb.dft9@gmail.com>
2020-01-08 08:30:07 +0000

    [Finish] 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f732875848d9f88a4c29c0298bc43878d9898b9bd136a8a5ccad066f4b0eaae17f5c1bfc8afab41f89692a711980b17af390daf6a08d2c740d6192420de6ed6b'
